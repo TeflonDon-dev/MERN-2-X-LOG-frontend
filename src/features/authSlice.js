5b7e5ab294b0a46f39129e081af214eb2e807b68
Joshua Okorie <joshuaokorie008@gmail.com>
2023-09-05 07:02:31 +0100

    error fix

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -17,10 +17,10 @@ const initialState = {
     userLoaded:false,
 }
 
-export const signUpUser = createAsyncThunk("auth/signUpUser", async (user, { rejectWithValue }) => {
+export const signUpUser = createAsyncThunk("/signUpUser", async (user, { rejectWithValue }) => {
     try {
 
-        const token = await axios.post(`${import.meta.env.VITE_SERVER_DOMAIN}/api/signup`, {
+        const token = await axios.post(`${import.meta.env.VITE_SERVER_DOMAIN}/signup`, {
             firstname: user.firstname,
             lastname: user.lastname,
             email: user.email,
@@ -40,9 +40,9 @@ export const signUpUser = createAsyncThunk("auth/signUpUser", async (user, { rej
 
 
 
-export const loginUser = createAsyncThunk("auth/loginUser", async (user, { rejectWithValue }) => {
+export const loginUser = createAsyncThunk("/loginUser", async (user, { rejectWithValue }) => {
     try {
-        const token = await axios.post(`${import.meta.env.VITE_SERVER_DOMAIN}/api/login`, {
+        const token = await axios.post(`${import.meta.env.VITE_SERVER_DOMAIN}/login`, {
             email: user.email,
             password: user.password,
             
