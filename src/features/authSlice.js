023d8d7bf25e3f71b984683c21c42c7708273cd5
Joshua Okorie <joshuaokorie008@gmail.com>
2023-09-05 04:55:32 +0100

    adjustments

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -20,7 +20,7 @@ const initialState = {
 export const signUpUser = createAsyncThunk("auth/signUpUser", async (user, { rejectWithValue }) => {
     try {
 
-        const token = await axios.post(`${import.meta.env.VITE_SERVER_DOMAIN}/signup`, {
+        const token = await axios.post(`${import.meta.env.VITE_SERVER_DOMAIN}/api/signup`, {
             firstname: user.firstname,
             lastname: user.lastname,
             email: user.email,
@@ -42,7 +42,7 @@ export const signUpUser = createAsyncThunk("auth/signUpUser", async (user, { rej
 
 export const loginUser = createAsyncThunk("auth/loginUser", async (user, { rejectWithValue }) => {
     try {
-        const token = await axios.post(`${import.meta.env.VITE_SERVER_DOMAIN}/login`, {
+        const token = await axios.post(`${import.meta.env.VITE_SERVER_DOMAIN}/api/login`, {
             email: user.email,
             password: user.password,
             
